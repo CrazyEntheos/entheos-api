d27ea7afcfe9fd92132c2414e4a0197284704e82
sg709c <sg709c@INHYI0089.intl.att.com>
2018-07-25 16:35:05 +0530

    [API-0004] - Updated product images and data for ui

--- src/main/java/com/entheos/store/api/StoreApplication.java
+++ src/main/java/com/entheos/store/api/StoreApplication.java
@@ -98,25 +98,25 @@ public class StoreApplication implements CommandLineRunner{
 		Category kidsCategory = categoryRepository.insert(
 				Category.builder().categoryId(String.valueOf(++categoryId)).categoryName("Kids").description("Kids wear").build());
 
-		Product p1 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("UCB Men's Cotton T-Shirt").description("Black Round Neck Full Sleeve T-Shirt")
+		Product p1 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Mebaz").description("Mebaz Kurtha")
 				.category(mensCategory).price(1500F).quantity(5).size(Arrays.asList("L")).brand("UCB").build());
 
-		Product p2 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("U.S. Polo Assn. Cotton Polo").description("U.S. Polo Assn. Men's Solid Regular Fit Cotton Polo")
+		Product p2 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("U.S. Polo Assn.").description("U.S. Polo Assn. Men's Cotton Shirt")
 				.category(mensCategory).price(1345F).quantity(5).size(Arrays.asList("S")).brand("U.S Polo").build());
 
-		Product p3 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("WROGN T-shirt").description("WROGN Men Grey & Rust Brown Striped Polo Collar T-shirt")
+		Product p3 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("WROGN T-shirt").description("WROGN Men Grey Polo T-shirt")
 				.category(mensCategory).price(3000F).quantity(5).size(Arrays.asList("M")).brand("WROGN").build());
 
-		Product p4 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("United Colors of Benetton Men").description("United Colors of Benetton Men Beige & Blue Striped Polo Collar T-shirt")
+		Product p4 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("VOI Jeans").description("VOI Jeans Blue")
 				.category(mensCategory).price(1500F).quantity(5).size(Arrays.asList("S")).brand("United Colors").build());
 
-		Product p5 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Puma T-shirt").description("Puma Men's Cotton Polo")
+		Product p5 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("ZARA").description("ZARA Men's Cotton Shirt")
 				.category(mensCategory).price(719F).quantity(5).size(Arrays.asList("XL")).brand("Puma").build());
 
 		Product p6 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Reebok T-shirt").description("Reebok Men's Cotton Polo")
 				.category(mensCategory).price(1500F).quantity(5).size(Arrays.asList("M")).brand("Reebok").build());
 
-		Product p7 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Adidas T-shirt").description("Adidas Men's Cotton Polo")
+		Product p7 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Cottonworld").description("Cottonworld Pant")
 				.category(mensCategory).price(719F).quantity(5).size(Arrays.asList("SL")).brand("Adidas").build());
 
 		Product p8 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Fila T-shirt").description("Fila Men's Cotton Polo")
@@ -125,56 +125,58 @@ public class StoreApplication implements CommandLineRunner{
 		mensCategory.setProducts(Arrays.asList(p1, p2, p3 ,p4 ,p5, p6, p7, p8));
 		categoryRepository.save(mensCategory);
 
-		Product p9= productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("W Ethnics").description("W Women Orange & Beige Printed Straight Kurta")
+		Product p9= productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Mebaz").description("Mebaz Yellow Straight Kurta")
 				.category(womensCategory).price(1250F).quantity(5).size(Arrays.asList("L")).brand("W").build());
 
-		Product p10= productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("AKS Dress").description("Biba Women Green Solid Kurta with Churidar & Dupatta")
+		Product p10= productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("W").description("W Women Red Kurta")
 				.category(womensCategory).price(2400F).quantity(5).size(Arrays.asList("S")).brand("AKS").build());
 
-		Product p11= productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("AKS Dress").description("AKS Women Green & White Printed Maxi Dress")
+		Product p11= productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("AND").description("AND Women Peach Kurtha")
 				.category(womensCategory).price(1125F).quantity(5).size(Arrays.asList("S")).brand("AKS").build());
 
-		Product p12= productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("W For Women").description("W Women Turquoise Blue & Coral Orange Printed Straight Kurta")
+		Product p12= productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("W For Women").description("W For Women Pink Dress")
 				.category(womensCategory).price(3056F).quantity(5).size(Arrays.asList("L")).brand("W").build());
 
-		Product p13 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("HERE&NOW Top").description("HERE&NOW Women Black Lightweight Floral Print Cold Shoulder Top")
+		Product p13 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Weavers India").description("Weavers India Dress Material")
 				.category(womensCategory).price(1455F).quantity(5).size(Arrays.asList("L")).brand("HERE&NOW Top").build());
 
-		Product p14 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("THERE&THEN Top").description("THERE&THEN  Women Black Lightweight Floral Print Cold Shoulder Top")
+		Product p14 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Kalanjali").description("Kalanjali Yellow Dress Material")
 				.category(womensCategory).price(1500F).quantity(5).size(Arrays.asList("XL")).brand("THERE&THEN Top").build());
 
-		Product p15 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("THEN&THERE Top").description("THEN&THERE Women White Lightweight Floral Print Cold Shoulder Top")
+		Product p15 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Globaldesi").description("Globaldesi Grey Top")
 				.category(womensCategory).price(1455F).quantity(5).size(Arrays.asList("L")).brand("THEN&THERE  Top").build());
 
-		Product p16 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("NOW Top").description("NOW Women Yellow Lightweight Floral Print Cold Shoulder Top")
+		Product p16 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("AND").description("AND Yellow Lightweight Top")
 				.category(womensCategory).price(1500F).quantity(5).size(Arrays.asList("SL")).brand("NOW Top").build());
 
 		womensCategory.setProducts(Arrays.asList(p9, p10, p11 ,p12 ,p13, p14, p15 ,p16));
 		categoryRepository.save(womensCategory);
 
-		Product p17 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("CUTECUMBER Girl's Dress").description("CUTECUMBER Girls Pink Self Design Fit and Flare Dress")
+		/* Product p17 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("CUTECUMBER Girl's Dress").description("CUTECUMBER Girls Pink Self Design Fit and Flare Dress")
+				.category(kidsCategory).price(1260F).quantity(5).size(Arrays.asList("L")).brand("CUTECUMBER").build()); */
+		
+		Product p17 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("UCB").description("UCB Black T Shirt Boys")
 				.category(kidsCategory).price(1260F).quantity(5).size(Arrays.asList("L")).brand("CUTECUMBER").build());
-
-		Product p18 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("StyleStone Girl's Dress").description("StyleStone Girls Blue & Off-White Printed A-Line Dress")
+		Product p18 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Mebaz").description("Mebaz Kurta Boys")
 				.category(kidsCategory).price(5045F).quantity(5).size(Arrays.asList("S")).brand("StyleStone").build());
 
-		Product p19 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("My Little Lambs Girl's Dress").description("My Little Lambs Girls Beige Printed Empire Dress")
+		Product p19 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Pepe Jeans").description("Pepe jeans T shirt Boys")
 				.category(kidsCategory).price(2600F).quantity(5).size(Arrays.asList("M")).brand("My Little Lambs").build());
 
-		Product p20 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("LilPicks Boys").description("LilPicks Boys Maroon & Navy Blue Solid T-shirt with Shorts")
+		Product p20 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Mebaz Girls").description("Mebaz Girls Printed Jeans")
 				.category(kidsCategory).price(1500F).quantity(5).size(Arrays.asList("L")).brand("LilPicks").build());
 
-		Product p21 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Mothercare Girls Cardigan").description("Mothercare Girls Yellow Self-Striped Cardigan")
+		Product p21 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Mothercare").description("Mothercare Girls Cardigan")
 				.category(kidsCategory).price(3015F).quantity(5).size(Arrays.asList("S")).brand("Mothercare").build());
 
-		Product p22 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Fathercare Girls Cardigan").description("Fathercare Girls Yellow Self-Striped Cardigan")
+		Product p22 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Princess").description("Princess Girls Green Frock")
 				.category(kidsCategory).price(3015F).quantity(5).size(Arrays.asList("L")).brand("Fathercare").build());
 
-		Product p23 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Dadcare Girls Cardigan").description("Dadcare Girls White Self-Striped Cardigan")
+		Product p23 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Mebaz Boys").description("Mebaz Boys Kurtha")
 				.category(kidsCategory).price(2600F).quantity(5).size(Arrays.asList("SL")).brand("Dadcare").build());
 
-		Product p24 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("Grannycare Girls Cardigan").description("Grannycare Girls Pink Self-Striped Cardigan")
-				.category(kidsCategory).price(3015F).quantity(5).size(Arrays.asList("XXL")).brand("Mothercare").build());
+		Product p24 = productRepository.save(Product.builder().productId(String.valueOf(++productId)).productName("H&M").description("H&M Boys Shorts")
+				.category(kidsCategory).price(3015F).quantity(5).size(Arrays.asList("L")).brand("Mothercare").build());
 
 		kidsCategory.setProducts(Arrays.asList(p17, p18, p19 ,p20 ,p21, p22, p23, p24));
 		categoryRepository.save(kidsCategory);
